42c6babf410d94db417d8152888586972ce4cb3b
Hanh Nguyen <hanhnguyen.learning@gmail.com>
2023-08-31 15:55:25 +0700

    > Config Proxy part 1 > Change app router to pages router > Refactor structure > Create material pages

--- pages/_redux/services/axiosConfig.ts
+++ pages/_redux/services/axiosConfig.ts
@@ -1,33 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com/",
+  baseURL: "api",
 });
-
-// Add a request interceptor
-axiosConfig.interceptors.request.use(
-  function (config) {
-    // // Modify the request config here (e.g., add headers, authentication tokens)
-    // const accessToken = JSON.parse(localStorage.getItem("token"));
-
-    // // ** If token is present add it to request's Authorization Header
-    // if (accessToken) {
-    //   if (config.headers) config.headers.token = accessToken;
-    // }
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
-);
-
-axiosConfig.interceptors.response.use(
-  function (response) {
-    return response;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
-);
 
 export default axiosConfig;
