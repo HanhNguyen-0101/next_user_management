531d676eccccd7da03acf60ce48864fcf8816219
Hanh Nguyen <hanhnguyen.learning@gmail.com>
2023-08-31 16:05:56 +0700

    Revert "Revert "Revert "> Config Proxy part 1"""
    This reverts commit 30258be33505621867769871f104fc9517fe1923.

--- pages/_redux/services/axiosConfig.ts
+++ pages/_redux/services/axiosConfig.ts
@@ -1,7 +1,33 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "api",
+  baseURL: "https://jsonplaceholder.typicode.com/",
 });
+
+// Add a request interceptor
+axiosConfig.interceptors.request.use(
+  function (config) {
+    // // Modify the request config here (e.g., add headers, authentication tokens)
+    // const accessToken = JSON.parse(localStorage.getItem("token"));
+
+    // // ** If token is present add it to request's Authorization Header
+    // if (accessToken) {
+    //   if (config.headers) config.headers.token = accessToken;
+    // }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axiosConfig.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosConfig;
